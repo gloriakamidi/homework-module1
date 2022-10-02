6626bc62b268158b393c860eed09277fc4a5cb7f
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-10-02 01:44:20 -0700

    jquery

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
  $(document).bind("click", function(e) {
-   $(e.target).closest("td").toggleClass("highlight").siblings(".none").removeClass("highlight");
+   $(e.target).closest("td").toggleClass("highlight");
+   $("td").filter(".none").removeClass("highlight");
  });
-   //$("td").filter(".none").removeClass("highlight");
 });
+
