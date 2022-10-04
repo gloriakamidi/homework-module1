2554191c8f977f5f3df851c0a204977b9a26928c
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-10-04 14:35:16 -0700

    box

--- js/activity.js
+++ js/activity.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
  }); 
   //User interaction with table cells
   $("td").click(function () {
-    // user select a tabke data cell
+    // user select a table data cell
     var content = $(this).text(); // get content of cell
 
     if (content != "Not Available") {
@@ -16,7 +16,8 @@ $(document).ready(function(){
         //check if selected cell has class
         $("#displaySelected").css("visibility", "visible"); //make display box visible
         $("#displaySelected").css("margin-top", "2em"); //add spaces above display box
-        $("#result").append("<p>" + content + "</p>"); //add child element with contents of cell
+        $("#result").append("<p>" + content + ' at' + ' $("td:eq(0)").text(index))' + "</p>" ); //add child element with contents of cell
+        //$("#result").append("")
       } else {
         // if selected cell don't have class
         $("#result p:contains(" + content + ")").remove(); //remove child element
